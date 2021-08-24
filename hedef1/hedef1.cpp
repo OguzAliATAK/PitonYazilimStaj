a8c8973534eed8777958b2ef6c7bd80c69abe1bc
OguzAliATAK <87567601+OguzAliATAK@users.noreply.github.com>
2021-08-24 11:43:47 +0300

    Add files via upload

---
+++ hedef1/hedef1.cpp
@@ -0,0 +1,108 @@
+﻿#include<iostream>
+#include<string>
+#include<locale>
+#include<math.h>
+using namespace std;
+
+
+
+void girileceksayilar(int a)
+{
+
+	
+	string str1 = ":";
+	string str2;
+	int fonk;
+	int deger;
+	fonk = 0;
+
+
+	cout << a << str1;
+	cout << endl;
+	int  dizi2[6];
+
+
+	cout << "Fonksiyon :   "  << "dizi2[0] - dizi2[1] + (dizi2[2] * dizi2[3]) / (dizi2[4] * dizi2[5])" << endl;
+	cout << "Fonksiyon :   "  << "(1.Değer - 2.Değer + (3.Değer] * 4.Değer]) / (5.Değer * 6.Değer))" << endl;
+	
+
+	
+
+	for (int  i = 0; i < 6; i++)
+	{
+		 
+		cout << i + 1 << ". değeri giriniz : ";
+		cin >> dizi2[i];
+
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		cout << endl;
+		cout << i + 1 << ". değeri: "<< dizi2[i] ;
+
+	}
+	fonk = (dizi2[0] - dizi2[1] + (dizi2[2] * dizi2[3]) / (dizi2[4] * dizi2[5]));
+
+	cout << endl;
+	cout << "Fonksiyonun değeri : ";
+	cout << fonk;
+	cout << endl;
+
+	if (a != fonk)
+	{
+		cout << "************" << endl;
+		cout << "Girdiğiniz sayılar fonksayona göre hedef sayıya eşit değil. " << endl;
+		cout << "Girdiğiniz değerlere göre fonksiyonun değeri : " << fonk << endl;
+		cout << "************" << endl;
+	}
+
+	if (a == fonk)
+	{
+		cout << "************" << endl;
+		cout << "Girdiğiniz sayılar fonksayona göre hedef sayıya eşittir. " << endl;
+		cout << "************" << endl;
+	}
+
+
+
+	
+	
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int hedefsayi;
+	int islem;
+	string toplam;
+a:
+
+	
+
+	
+	cout << "Hedef sayıyı giriniz :";
+	cin >> hedefsayi;
+	girileceksayilar(hedefsayi);
+
+	cout << "Yeniden değer girmek için 1'e basınız." << endl;
+	cout << "Çıkmak için herhangi bir tuşa basınız";
+	cin >> islem;
+	switch (islem)
+	{
+	case 1: system("CLS");  goto a;
+	default:
+		break;
+	}
+
+	cout << endl;
+
+	system("PAUSE");
+
+
+
+
+
+
+}
