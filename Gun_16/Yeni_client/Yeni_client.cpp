814983ecc1c9cf33051972a7ed530427994893c7
OguzAliATAK <oaliatak@hotmail.com>
2021-09-14 18:55:04 +0300

    Gun_16
    Veri tabani tekrari sorunu cozuldu.Client tarafindan gonderilen bilgilere gore server tarafindan islem yapilabiliyor.2 Karakterden fazla veri gonderilemiyor.

---
+++ Gun_16/Yeni_client/Yeni_client.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<WS2tcpip.h>
+#include<string>
+#include<Windows.h>
+#include"Musteri_cl.h"
+
+#pragma comment(lib, "ws2_32.lib ")
+
+using namespace std;
+
+int main()
+{
+	RECT r;
+	HWND console = GetConsoleWindow();
+	GetWindowRect(console, &r); //stores the console's current dimensions
+	MoveWindow(console, r.left, r.top, 600, 500, TRUE);
+
+	
+	Client1 clienta;
+
+	clienta.soket_baslat();
+	clienta.soket_islem();
+	clienta.soket_kapat();
+
+	system("pause");
+	return 0;
+}
